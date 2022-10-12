586dbaae5826bc965c2fc03b80b5395e453f1051
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2022-10-12 11:51:20 -0300

    actualizado

--- FT-M2-master/11-Redux/hw-js/src/reducer/index.js
+++ FT-M2-master/11-Redux/hw-js/src/reducer/index.js
@@ -9,7 +9,25 @@ const initialState = {
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
-  
+  switch (action.type) {
+    case 'INCREMENTO':
+      return ({
+        contador: state.contador + 1
+      })
+
+
+    case 'DECREMENTO':
+      return ({
+        contador: state.contador - 1
+      })
+
+
+
+    default:
+      return state
+
+      type: INCREMENT;
+  }
 }
 
 module.exports = contador;
